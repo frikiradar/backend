23c8f2b85ff35d9f482277ec36ad1fdfe5e4a2f9
Alberto Rodríguez Merino <alberto.rodriguez@altafonte.com>
2019-03-02 16:59:16 +0100

    de nuevo gd...

--- src/Service/FileUploader.php
+++ src/Service/FileUploader.php
@@ -4,7 +4,7 @@ namespace App\Service;
 
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Imagine\Imagick\Imagine;
+use Imagine\Gd\Imagine;
 
 class FileUploader
 {
