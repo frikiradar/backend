ab9614274905a2647ca1b227d58a95d563ea360d
Alberto Rodríguez Merino <alberto.rodriguez@altafonte.com>
2019-03-15 10:04:31 +0100

    fix query

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -126,7 +126,7 @@ class UserRepository extends ServiceEntityRepository implements UserLoaderInterf
             ))
             ->andHaving('distance <= :ratio')
             ->andHaving('age BETWEEN :minage AND :maxage')
-            ->andWhere(count($user->getLovegender()) > 0 ? 'u.gender IN (:lovegender)' : 'u.gender <> 1')
+            ->andWhere($user->getLovegender() ? 'u.gender IN (:lovegender)' : 'u.gender <> :lovegender')
             // ->andWhere('u.connection IN (:connection)')
             ->andWhere('u.id <> :id')
             ->andWhere("u.roles NOT LIKE '%ROLE_ADMIN%'")
@@ -136,7 +136,7 @@ class UserRepository extends ServiceEntityRepository implements UserLoaderInterf
                 'minage' => $user->getMinage() ?: 18,
                 'maxage' => $user->getMaxage() ?: 99,
                 'id' => $user->getId(),
-                'lovegender' => $user->getLovegender(),
+                'lovegender' => $user->getLovegender() ?: 1,
                 // 'connection' => $user->getConnection()
             ))
             ->getQuery()
