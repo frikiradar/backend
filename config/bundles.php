d790b919a4afaf919d191c9b20f8d49c6616a1d4
Alberto Rodríguez Merino <alberto.rodriguez@altafonte.com>
2019-03-16 20:20:01 +0100

    fix

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
