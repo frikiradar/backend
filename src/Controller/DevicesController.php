2b3a57b8bd7f3fd0b1d97ecffb3f8864338d347f
Alberto Rodríguez Merino <alberto.rodriguez@altafonte.com>
2019-04-05 00:03:53 +0200

    fix unknown device

--- src/Controller/DevicesController.php
+++ src/Controller/DevicesController.php
@@ -15,6 +15,7 @@ namespace App\Controller;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
