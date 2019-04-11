664e8625d7d57bdad7c684f96b07052fa07e5d84
Alberto Rodríguez Merino <alberto.rodriguez@altafonte.com>
2019-04-11 23:38:56 +0200

    test click and sound

--- src/Repository/NotificationRepository.php
+++ src/Repository/NotificationRepository.php
@@ -101,7 +101,7 @@ class NotificationRepository extends ServiceEntityRepository
                         'notification' => [
                             'title' => $title,
                             'body' => $text,
-                            'sound' => 'bipbip',
+                            'sound' => "bipbip.mp3",
                             'click_action' => "FCM_PLUGIN_ACTIVITY"
                         ],
                     ]);
