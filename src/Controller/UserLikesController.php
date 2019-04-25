97803239bd8ba87d2ef9f9a47b207beae049feaf
Alberto Rodríguez Merino <alberto.rodriguez@altafonte.com>
2019-04-25 14:21:03 +0200

    fix userlike

--- src/Controller/UserLikesController.php
+++ src/Controller/UserLikesController.php
@@ -2,19 +2,16 @@
 // src/Controller/ChatController.php
 namespace App\Controller;
 
-use App\Entity\UserLike;
+use App\Entity\LikeUser;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
-use FOS\RestBundle\Request\ParamFetcherInterface;
 use JMS\Serializer\SerializationContext;
 use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Swagger\Annotations as SWG;
-use Symfony\Component\Mercure\Publisher;
-use Symfony\Component\Mercure\Update;
-use App\Entity\LikeUser;
 
 /**
  * Class UserLikesController
