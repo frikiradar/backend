5a5f174137d82f36d3f0afa9db7f0f4bb9d62bc3
Alberto Rodríguez Merino <alberto.rodriguez@altafonte.com>
2019-07-31 00:03:29 +0200

    fix registro

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -150,6 +150,7 @@ class UsersController extends FOSRestController
                 $user->setRegisterIp();
                 $user->setActive(false);
                 $user->setTwoStep(false);
+                $user->setMailing($request->request->get('mailing') ?: false);
                 $user->setVerificationCode();
                 $user->setRoles(['ROLE_USER']);
 
