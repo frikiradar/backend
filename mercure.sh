b22adae283d90864774b66a3695ffc4f4d602e54
Alberto Rodríguez Merino <alberto.rodriguez@altafonte.com>
2019-08-14 23:04:20 +0200

    test mercure

--- mercure.sh
+++ mercure.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-JWT_KEY='FrikiChat' ADDR='dev.frikiradar.com:3000' CORS_ALLOWED_ORIGINS=* ALLOW_ANONYMOUS=1 ./mercure/mercure
+JWT_KEY='FrikiChat' ADDR='app.frikiradar.com:3000' CORS_ALLOWED_ORIGINS=* ALLOW_ANONYMOUS=1 ./mercure/mercure
