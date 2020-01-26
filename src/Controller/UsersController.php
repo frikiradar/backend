70b530da2c0f8f83d3bd596aeeeb77b879309243
Alberto Rodríguez Merino <alberto.rodriguez@altafonte.com>
2020-01-26 16:44:49 +0100

    test

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -661,45 +661,7 @@ class UsersController extends FOSRestController
         }
     }
 
-    /**
-     * @Rest\Get("/v1/radar/")
-     *
-     * @SWG\Response(
-     *     response=201,
-     *     description="Coordenadas actualizadas correctamente"
-     * )
-     *
-     * @SWG\Response(
-     *     response=500,
-     *     description="Error al actualizar las coordenadas"
-     * )
-     * 
-     * @Rest\QueryParam(
-     *     name="page",
-     *     default="1",
-     *     description="Radar page"
-     * )
-     * 
-     */
-    public function getRadarUsers(ParamFetcherInterface $params)
-    {
-        ini_set('max_execution_time', 60);
-        ini_set('memory_limit', '512M');
 
-        $serializer = $this->get('jms_serializer');
-        $em = $this->getDoctrine()->getManager();
-
-        $page = $params->get("page");
-
-        try {
-
-            $users = $em->getRepository('App:User')->getRadarUsers($this->getUser(), $page);
-
-            return new Response($serializer->serialize($users, "json", SerializationContext::create()->setGroups(array('default'))));
-        } catch (Exception $ex) {
-            throw new HttpException(400, "Error al obtener los usuarios - Error: {$ex->getMessage()}");
-        }
-    }
 
     /**
      * @Rest\Post("/v1/search")
