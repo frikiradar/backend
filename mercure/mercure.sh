8477decd8d4937aa05ff26afb7902b9abcfa0310
Alberto Rodriguez <alberto.rodriguez@altafonte.com>
2020-05-30 18:20:07 +0200

    tst

--- mercure/mercure.sh
+++ mercure/mercure.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-JWT_KEY='FrikiChat' ADDR='frikiradar.com:3000' CORS_ALLOWED_ORIGINS='*' ALLOW_ANONYMOUS=1 ACME_HOSTS='frikiradar.com' ./mercure
+JWT_KEY='FrikiChat' CORS_ALLOWED_ORIGINS='*' ALLOW_ANONYMOUS=1 ACME_HOSTS='frikiradar.com' ./mercure
