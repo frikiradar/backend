33bd6ba062c81c1bc99670d6614b33946c93a877
Alberto Rodriguez <alberto.rodriguez@altafonte.com>
2021-04-01 15:55:43 -0600

    set page update

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Page;
 use App\Entity\Tag;
 use App\Repository\TagRepository;
 use App\Service\AccessCheckerService;
@@ -64,8 +65,123 @@ class PagesController extends AbstractController
         try {
             $tag = $this->em->getRepository('App:Tag')->findOneBy(array('id' => $this->request->get($request, 'id')));
             $name = $tag->getName();
-            $page = $this->em->getRepository('App:Tag')->createPage($name);
-            return new Response($this->serializer->serialize($page, "json", ['groups' => 'default']));
+            $search = trim(str_replace('Saga', '', $name));
+            $search = str_replace(': ', ' ', $search);
+            // $this->o->writeln($search);
+            $clientId = '1xglmlbz31omgifwlnjzfjjw5bukv9';
+            $clientSecret = 'niozz7jpskr27vr9c5v1go801q3wsz';
+            $url = 'https://id.twitch.tv/oauth2/token?client_id=' . $clientId . '&client_secret=' . $clientSecret . '&grant_type=client_credentials';
+            $ch = curl_init($url); // Initialise cURL
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POST, 1); // Specify the request method as POST
+            $output = curl_exec($ch); // Execute the cURL statement
+            curl_close($ch); // Close the cURL connection
+            $info = json_decode($output, true); // Return the received data
+
+            $url = 'https://api.igdb.com/v4';
+            $endpoint = '/games';
+            $bearer = "Authorization: Bearer " . $info['access_token']; // Prepare the authorisation token
+            $client_id = "Client-ID: " . $clientId;
+            // $body = 'search "' . $search . '"; fields name, cover.url, game_modes.slug, multiplayer_modes.*, rating, slug, summary, first_release_date, artworks.*; where version_parent = null; limit 500;';
+            // $body = 'fields name, cover.url, game_modes.slug, multiplayer_modes.*, rating, slug, summary, first_release_date, artworks.*; where name ~ "' . $search . '" & version_parent = null; limit 500;';
+            // $body = 'fields name, cover.url, game_modes.slug, multiplayer_modes.*, rating, slug, summary, first_release_date, artworks.*; where name ~ *"' . $search . '"* & version_parent = null; limit 500;';
+            $body = 'fields name, cover.url, game_modes.slug, multiplayer_modes.*, rating, slug, summary, first_release_date, artworks.*; where slug ~ *"' . str_replace([':', ' '], '-', strtolower($search)) . '"* & version_parent = null; limit 500;';
+            // $this->o->writeln($body);
+            $ch = curl_init($url . $endpoint); // Initialise cURL
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array($bearer, $client_id)); // Inject the token into the header
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POST, 1); // Specify the request method as POST
+            $output = curl_exec($ch); // Execute the cURL statement
+            curl_close($ch); // Close the cURL connection
+            $games = json_decode($output, true); // Return the received data
+            usort($games, function ($a, $b) {
+                return (isset($a['first_release_date']) ? $a['first_release_date'] : 99999999999) <=> (isset($b['first_release_date']) ? $b['first_release_date'] : 99999999999);
+            });
+            // print_r($games);
+            $gameFound = [];
+
+            foreach ($games as $game) {
+                similar_text(strtolower($game['name']), strtolower($search), $percent);
+                if ($percent >= 98) {
+                    $this->o->writeln($percent);
+                    $gameFound = $game;
+                    break;
+                }
+            }
+
+            if (!empty($gameFound)) {
+                $game = $gameFound;
+            } else {
+                $game = $games[0];
+            }
+
+            if (isset($game['artworks'][0]['url'])) {
+                $game['artworks'][0]['url'] = str_replace('t_thumb', 't_screenshot_med', $game['artworks'][0]['url']);
+            }
+            if (isset($game['cover']['url']) && !isset($game['artworks'][0]['url'])) {
+                $game['artworks'][0]['url'] = str_replace('t_thumb', 't_screenshot_med', $game['cover']['url']);
+            }
+            if (isset($game['cover']['url'])) {
+                $game['cover']['url'] = str_replace('t_thumb', 't_cover_big', $game['cover']['url']);
+            }
+
+            if (isset($game['multiplayer_modes'][0]['onlinecoop']) && $game['multiplayer_modes'][0]['onlinecoop'] == 1) {
+                $game['game_mode'] = 'online';
+            }
+
+            if (isset($game['game_modes'])) {
+                foreach ($game['game_modes'] as $gameMode) {
+                    if ($gameMode['slug'] === 'massively-multiplayer-online-mmo') {
+                        $game['game_mode'] = 'online';
+                        break;
+                    }
+                }
+            }
+
+            if (!isset($game['game_mode'])) {
+                $game['game_mode'] = 'offline';
+            }
+
+            if (isset($game)) {
+                /**
+                 * @var Page
+                 */
+                $page = new Page();
+                $page->setName($game['name']);
+                $page->setDescription($game['summary']);
+                $page->setSlug($game['slug']);
+                $page->setRating($game['rating']);
+                $page->setReleaseDate(new \DateTime($game['first_release_date']));
+                $page->setTimeCreation();
+                $page->setLastUpdate();
+                $page->setGameMode($game['game_mode']);
+
+                $server = "https://app.frikiradar.com";
+                if (isset($game['cover'])) {
+                    // Obtenemos cover y la subimos tal cual está
+                    $file = '/var/www/vhosts/frikiradar.com/app.frikiradar.com/images/pages/' . $game['slug'] . '/cover.jpg';
+                    if (file_put_contents($file, file_get_contents('https:' . $game['cover']['url']))) {
+                        $src = str_replace("/var/www/vhosts/frikiradar.com/app.frikiradar.com", $server, $file);
+                        $page->setCover($src);
+                    }
+                }
+
+                if (isset($game['artworks'][0])) {
+                    // Obtenemos cover y la subimos tal cual está
+                    $file = '/var/www/vhosts/frikiradar.com/app.frikiradar.com/images/pages/' . $game['slug'] . '/artwork.jpg';
+                    if (file_put_contents($file, file_get_contents('https:' . $game['artworks'][0]['url']))) {
+                        $src = str_replace("/var/www/vhosts/frikiradar.com/app.frikiradar.com", $server, $file);
+                        $page->setArtwork($src);
+                    }
+                }
+
+                $this->em->persist($page);
+                $this->em->flush();
+                return new Response($this->serializer->serialize($page, "json", ['groups' => 'default']));
+            } else {
+                throw new HttpException(400, "Error al crear la página");
+            }
         } catch (Exception $ex) {
             throw new HttpException(400, "Error al crear la página - Error: {$ex->getMessage()}");
         }
--- src/Repository/PageRepository.php
+++ src/Repository/PageRepository.php
@@ -4,9 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Page;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
-use Exception;
 
 /**
  * @method Page|null find($id, $lockMode = null, $lockVersion = null)
@@ -16,10 +14,9 @@ use Exception;
  */
 class PageRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry, EntityManagerInterface $entityManager)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Page::class);
-        $this->em = $entityManager;
     }
 
     // /**
@@ -50,126 +47,4 @@ class PageRepository extends ServiceEntityRepository
         ;
     }
     */
-
-    public function createPage(string $query)
-    {
-        $search = trim(str_replace('Saga', '', $query));
-        $search = str_replace(': ', ' ', $search);
-        // $this->o->writeln($search);
-        $clientId = '1xglmlbz31omgifwlnjzfjjw5bukv9';
-        $clientSecret = 'niozz7jpskr27vr9c5v1go801q3wsz';
-        $url = 'https://id.twitch.tv/oauth2/token?client_id=' . $clientId . '&client_secret=' . $clientSecret . '&grant_type=client_credentials';
-        $ch = curl_init($url); // Initialise cURL
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POST, 1); // Specify the request method as POST
-        $output = curl_exec($ch); // Execute the cURL statement
-        curl_close($ch); // Close the cURL connection
-        $info = json_decode($output, true); // Return the received data
-
-        $url = 'https://api.igdb.com/v4';
-        $endpoint = '/games';
-        $bearer = "Authorization: Bearer " . $info['access_token']; // Prepare the authorisation token
-        $client_id = "Client-ID: " . $clientId;
-        // $body = 'search "' . $search . '"; fields name, cover.url, game_modes.slug, multiplayer_modes.*, rating, slug, summary, first_release_date, artworks.*; where version_parent = null; limit 500;';
-        // $body = 'fields name, cover.url, game_modes.slug, multiplayer_modes.*, rating, slug, summary, first_release_date, artworks.*; where name ~ "' . $search . '" & version_parent = null; limit 500;';
-        // $body = 'fields name, cover.url, game_modes.slug, multiplayer_modes.*, rating, slug, summary, first_release_date, artworks.*; where name ~ *"' . $search . '"* & version_parent = null; limit 500;';
-        $body = 'fields name, cover.url, game_modes.slug, multiplayer_modes.*, rating, slug, summary, first_release_date, artworks.*; where slug ~ *"' . str_replace([':', ' '], '-', strtolower($search)) . '"* & version_parent = null; limit 500;';
-        // $this->o->writeln($body);
-        $ch = curl_init($url . $endpoint); // Initialise cURL
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array($bearer, $client_id)); // Inject the token into the header
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POST, 1); // Specify the request method as POST
-        $output = curl_exec($ch); // Execute the cURL statement
-        curl_close($ch); // Close the cURL connection
-        $games = json_decode($output, true); // Return the received data
-        usort($games, function ($a, $b) {
-            return (isset($a['first_release_date']) ? $a['first_release_date'] : 99999999999) <=> (isset($b['first_release_date']) ? $b['first_release_date'] : 99999999999);
-        });
-        // print_r($games);
-        $gameFound = [];
-
-        foreach ($games as $game) {
-            similar_text(strtolower($game['name']), strtolower($search), $percent);
-            if ($percent >= 98) {
-                $this->o->writeln($percent);
-                $gameFound = $game;
-                break;
-            }
-        }
-
-        if (!empty($gameFound)) {
-            $game = $gameFound;
-        } else {
-            $game = $games[0];
-        }
-
-        if (isset($game['artworks'][0]['url'])) {
-            $game['artworks'][0]['url'] = str_replace('t_thumb', 't_screenshot_med', $game['artworks'][0]['url']);
-        }
-        if (isset($game['cover']['url']) && !isset($game['artworks'][0]['url'])) {
-            $game['artworks'][0]['url'] = str_replace('t_thumb', 't_screenshot_med', $game['cover']['url']);
-        }
-        if (isset($game['cover']['url'])) {
-            $game['cover']['url'] = str_replace('t_thumb', 't_cover_big', $game['cover']['url']);
-        }
-
-        if (isset($game['multiplayer_modes'][0]['onlinecoop']) && $game['multiplayer_modes'][0]['onlinecoop'] == 1) {
-            $game['game_mode'] = 'online';
-        }
-
-        if (isset($game['game_modes'])) {
-            foreach ($game['game_modes'] as $gameMode) {
-                if ($gameMode['slug'] === 'massively-multiplayer-online-mmo') {
-                    $game['game_mode'] = 'online';
-                    break;
-                }
-            }
-        }
-
-        if (!isset($game['game_mode'])) {
-            $game['game_mode'] = 'offline';
-        }
-
-        if (isset($game)) {
-            /**
-             * @var Page
-             */
-            $page = new Page();
-            $page->setName($game['name']);
-            $page->setDescription($game['summary']);
-            $page->setSlug($game['slug']);
-            $page->setRating($game['rating']);
-            $page->setReleaseDate(new \DateTime($game['first_release_date']));
-            $page->setTimeCreation();
-            $page->setLastUpdate();
-            $page->setGameMode($game['game_mode']);
-
-            $server = "https://app.frikiradar.com";
-            if (isset($game['cover'])) {
-                // Obtenemos cover y la subimos tal cual está
-                $file = '/var/www/vhosts/frikiradar.com/app.frikiradar.com/images/pages/' . $game['slug'] . '/cover.jpg';
-                if (file_put_contents($file, file_get_contents('https:' . $game['cover']['url']))) {
-                    $src = str_replace("/var/www/vhosts/frikiradar.com/app.frikiradar.com", $server, $file);
-                    $page->setCover($src);
-                }
-            }
-
-            if (isset($game['artworks'][0])) {
-                // Obtenemos cover y la subimos tal cual está
-                $file = '/var/www/vhosts/frikiradar.com/app.frikiradar.com/images/pages/' . $game['slug'] . '/artwork.jpg';
-                if (file_put_contents($file, file_get_contents('https:' . $game['artworks'][0]['url']))) {
-                    $src = str_replace("/var/www/vhosts/frikiradar.com/app.frikiradar.com", $server, $file);
-                    $page->setArtwork($src);
-                }
-            }
-
-            $this->em->persist($page);
-            $this->em->flush();
-
-            return $page;
-        }
-
-        throw new Exception("Error al crear la página");
-    }
 }
