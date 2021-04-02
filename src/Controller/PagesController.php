4fc9adc131913105f599e03d0a1b1a5a84c5156f
Alberto Rodriguez <alberto.rodriguez@altafonte.com>
2021-04-01 20:02:03 -0600

    fix walking dead

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -67,6 +67,7 @@ class PagesController extends AbstractController
             $tag = $this->em->getRepository('App:Tag')->findOneBy(array('id' => $this->request->get($request, 'id')));
             $name = $tag->getName();
             $search = trim(str_replace('Saga', '', $name));
+            $search = str_replace('&', 'and', $search);
             $search = str_replace(': ', ' ', $search);
             $clientId = '1xglmlbz31omgifwlnjzfjjw5bukv9';
             $clientSecret = 'niozz7jpskr27vr9c5v1go801q3wsz';
@@ -94,101 +95,105 @@ class PagesController extends AbstractController
             $output = curl_exec($ch); // Execute the cURL statement
             curl_close($ch); // Close the cURL connection
             $games = json_decode($output, true); // Return the received data
-            usort($games, function ($a, $b) {
-                return (isset($a['first_release_date']) ? $a['first_release_date'] : 99999999999) <=> (isset($b['first_release_date']) ? $b['first_release_date'] : 99999999999);
-            });
-            // print_r($games);
-            $gameFound = [];
+            if (!empty($games)) {
+                usort($games, function ($a, $b) {
+                    return (isset($a['first_release_date']) ? $a['first_release_date'] : 99999999999) <=> (isset($b['first_release_date']) ? $b['first_release_date'] : 99999999999);
+                });
+                // print_r($games);
+                $gameFound = [];
 
-            foreach ($games as $game) {
-                similar_text(strtolower($game['name']), strtolower($search), $percent);
-                if ($percent >= 98) {
-                    $gameFound = $game;
-                    break;
-                }
-            }
-
-            if (!empty($gameFound)) {
-                $game = $gameFound;
-            } else {
-                $game = $games[0];
-            }
-
-            if (isset($game['artworks'][0]['url'])) {
-                $game['artworks'][0]['url'] = str_replace('t_thumb', 't_screenshot_med', $game['artworks'][0]['url']);
-            }
-            if (isset($game['cover']['url']) && !isset($game['artworks'][0]['url'])) {
-                $game['artworks'][0]['url'] = str_replace('t_thumb', 't_screenshot_med', $game['cover']['url']);
-            }
-            if (isset($game['cover']['url'])) {
-                $game['cover']['url'] = str_replace('t_thumb', 't_cover_big', $game['cover']['url']);
-            }
-
-            if (isset($game['multiplayer_modes'][0]['onlinecoop']) && $game['multiplayer_modes'][0]['onlinecoop'] == 1) {
-                $game['game_mode'] = 'online';
-            }
-
-            if (isset($game['game_modes'])) {
-                foreach ($game['game_modes'] as $gameMode) {
-                    if ($gameMode['slug'] === 'massively-multiplayer-online-mmo') {
-                        $game['game_mode'] = 'online';
+                foreach ($games as $game) {
+                    similar_text(strtolower($game['name']), strtolower($search), $percent);
+                    if ($percent >= 98) {
+                        $gameFound = $game;
                         break;
                     }
                 }
-            }
 
-            if (!isset($game['game_mode'])) {
-                $game['game_mode'] = 'offline';
-            }
-
-            if (isset($game)) {
-                /**
-                 * @var Page
-                 */
-                $page = new Page();
-                $page->setName($game['name']);
-                $page->setDescription($game['summary']);
-                $page->setSlug($game['slug']);
-                $page->setRating($game['rating']);
-                if (isset($game['first_release_date'])) {
-                    $date = new \DateTime();
-                    $date->setTimestamp($game['first_release_date']);
-                    $page->setReleaseDate($date);
+                if (!empty($gameFound)) {
+                    $game = $gameFound;
+                } else {
+                    $game = $games[0];
                 }
-                $page->setTimeCreation();
-                $page->setLastUpdate();
-                $page->setGameMode($game['game_mode']);
 
-                $server = "https://app.frikiradar.com";
-                $path = '/var/www/vhosts/frikiradar.com/app.frikiradar.com/images/pages/' . $game['slug'] . '/';
-                $fs = new Filesystem();
-                if (isset($game['cover'])) {
-                    $file =  'cover.jpg';
-                    if (!file_exists($path)) {
-                        mkdir($path, 0777, true);
+                if (isset($game['artworks'][0]['url'])) {
+                    $game['artworks'][0]['url'] = str_replace('t_thumb', 't_screenshot_med', $game['artworks'][0]['url']);
+                }
+                if (isset($game['cover']['url']) && !isset($game['artworks'][0]['url'])) {
+                    $game['artworks'][0]['url'] = str_replace('t_thumb', 't_screenshot_med', $game['cover']['url']);
+                }
+                if (isset($game['cover']['url'])) {
+                    $game['cover']['url'] = str_replace('t_thumb', 't_cover_big', $game['cover']['url']);
+                }
+
+                if (isset($game['multiplayer_modes'][0]['onlinecoop']) && $game['multiplayer_modes'][0]['onlinecoop'] == 1) {
+                    $game['game_mode'] = 'online';
+                }
+
+                if (isset($game['game_modes'])) {
+                    foreach ($game['game_modes'] as $gameMode) {
+                        if ($gameMode['slug'] === 'massively-multiplayer-online-mmo') {
+                            $game['game_mode'] = 'online';
+                            break;
+                        }
+                    }
+                }
+
+                if (!isset($game['game_mode'])) {
+                    $game['game_mode'] = 'offline';
+                }
+
+                if (isset($game)) {
+                    /**
+                     * @var Page
+                     */
+                    $page = new Page();
+                    $page->setName($game['name']);
+                    $page->setDescription($game['summary']);
+                    $page->setSlug($game['slug']);
+                    $page->setRating($game['rating']);
+                    if (isset($game['first_release_date'])) {
+                        $date = new \DateTime();
+                        $date->setTimestamp($game['first_release_date']);
+                        $page->setReleaseDate($date);
+                    }
+                    $page->setTimeCreation();
+                    $page->setLastUpdate();
+                    $page->setGameMode($game['game_mode']);
+
+                    $server = "https://app.frikiradar.com";
+                    $path = '/var/www/vhosts/frikiradar.com/app.frikiradar.com/images/pages/' . $game['slug'] . '/';
+                    $fs = new Filesystem();
+                    if (isset($game['cover'])) {
+                        $file =  'cover.jpg';
+                        if (!file_exists($path)) {
+                            mkdir($path, 0777, true);
+                        }
+
+                        $fs->appendToFile($path . $file, file_get_contents('https:' . $game['cover']['url']));
+                        $src = str_replace("/var/www/vhosts/frikiradar.com/app.frikiradar.com", $server, $path . $file);
+                        $page->setCover($src);
                     }
 
-                    $fs->appendToFile($path . $file, file_get_contents('https:' . $game['cover']['url']));
-                    $src = str_replace("/var/www/vhosts/frikiradar.com/app.frikiradar.com", $server, $path . $file);
-                    $page->setCover($src);
-                }
+                    if (isset($game['artworks'][0])) {
+                        $file = 'artwork.jpg';
+                        if (!file_exists($path)) {
+                            mkdir($path, 0777, true);
+                        }
 
-                if (isset($game['artworks'][0])) {
-                    $file = 'artwork.jpg';
-                    if (!file_exists($path)) {
-                        mkdir($path, 0777, true);
+                        $fs->appendToFile($path . $file, file_get_contents('https:' . $game['artworks'][0]['url']));
+                        $src = str_replace("/var/www/vhosts/frikiradar.com/app.frikiradar.com", $server, $path . $file);
+                        $page->setArtwork($src);
                     }
 
-                    $fs->appendToFile($path . $file, file_get_contents('https:' . $game['artworks'][0]['url']));
-                    $src = str_replace("/var/www/vhosts/frikiradar.com/app.frikiradar.com", $server, $path . $file);
-                    $page->setArtwork($src);
+                    $this->em->persist($page);
+                    $this->em->flush();
+                    return new Response($this->serializer->serialize($page, "json", ['groups' => 'default']));
+                } else {
+                    throw new HttpException(400, "Error al crear la página");
                 }
-
-                $this->em->persist($page);
-                $this->em->flush();
-                return new Response($this->serializer->serialize($page, "json", ['groups' => 'default']));
             } else {
-                throw new HttpException(400, "Error al crear la página");
+                throw new HttpException(400, "No se han obtenido resultados");
             }
         } catch (Exception $ex) {
             throw new HttpException(400, "Error al crear la página - Error: {$ex->getMessage()}");
