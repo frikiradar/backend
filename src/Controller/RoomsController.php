65a72fb06acf5593c7dfee79f2e9d3fda88a0dc5
Alberto Rodriguez <alberto.rodriguez@altafonte.com>
2021-04-05 16:56:17 -0500

    borrar caches rooms

--- src/Controller/RoomsController.php
+++ src/Controller/RoomsController.php
@@ -88,6 +88,7 @@ class RoomsController extends AbstractController
         $user = $this->getUser();
         $cache = new FilesystemAdapter();
         $roomCache = $cache->getItem('room.' . $slug);
+        $cache->deleteItem('room.' . $slug);
         try {
             if (!$roomCache->isHit()) {
                 $room = $this->em->getRepository('App:Room')->findOneBy(array('slug' => $slug));
