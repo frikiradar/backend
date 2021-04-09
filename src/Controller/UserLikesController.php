1d34cd81d800c5bccf39b3bbe4ced7212ba4c2ab
Alberto Rodriguez <alberto.rodriguez@altafonte.com>
2021-04-09 13:39:13 -0500

    fix fix!!

--- src/Controller/UserLikesController.php
+++ src/Controller/UserLikesController.php
@@ -110,7 +110,7 @@ class UserLikesController extends AbstractController
                 $user = $this->getUser();
             }
 
-            if ($user->getId() === $this->getUser()->getId() || !$user->getHideLikes() || !$this->security->isGranted('ROLE_MASTER')) {
+            if ($user->getId() === $this->getUser()->getId() || !$user->getHideLikes() || $this->security->isGranted('ROLE_MASTER')) {
                 $likesCache = $cache->getItem('users.likes.' . $user->getId() . $param . $page);
                 if (!$likesCache->isHit()) {
                     $likesCache->expiresAfter(5 * 60);
