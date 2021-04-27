4a21ad2c6855ab851ef708807d42724d5f595be2
Alberto Rodriguez <alberto.rodriguez@altafonte.com>
2021-04-27 18:14:29 -0500

    sacar contador de likes de cache de paginas

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -77,9 +77,6 @@ class PagesController extends AbstractController
                     $room->setVisible(false);
                     $room->setPermissions(['ROLE_USER']);
                     $page->setRoom($room);
-
-                    $likes = $this->em->getRepository('App:Tag')->countTag($page->getName(), $page->getCategory());
-                    $page->setLikes($likes['total']);
                     $pageCache->expiresAfter(3600 * 24);
                     $pageCache->set($page);
                     $cache->save($pageCache);
@@ -94,6 +91,9 @@ class PagesController extends AbstractController
             if (isset($messages[0])) {
                 $page->getRoom()->setLastMessage($messages[0]['last_message']);
             }
+
+            $likes = $this->em->getRepository('App:Tag')->countTag($page->getName(), $page->getCategory());
+            $page->setLikes($likes['total']);
 
             return new Response($this->serializer->serialize($page, "json", ['groups' => 'default', 'datetime_format' => 'Y-m-d', AbstractObjectNormalizer::SKIP_NULL_VALUES => true]));
         } catch (Exception $ex) {
