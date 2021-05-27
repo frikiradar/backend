03684526302ee73a195098d16b67746a4d9b2b57
Alberto Rodriguez <alberto.rodriguez@altafonte.com>
2021-05-27 10:23:59 -0500

    fix prevent duplicates devices

--- src/Repository/DeviceRepository.php
+++ src/Repository/DeviceRepository.php
@@ -54,10 +54,10 @@ class DeviceRepository extends ServiceEntityRepository
 
     public function set(User $user, string $id, string $name, string $token = "")
     {
-        $device = $this->findOneBy(array('device_name' => $name, 'user' => $user));
+        $device = $this->findOneBy(array('token' => $token, 'user' => $user));
 
-        if (empty($device) && !empty($token)) {
-            $device = $this->findOneBy(array('token' => $token, 'user' => $user));
+        if (empty($device)) {
+            $device = $this->findOneBy(array('device_name' => $name, 'user' => $user));
         }
 
         if (empty($device)) {
