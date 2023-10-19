f6a8e21085e6032388742602ebb8309555f2a7a2
Alberto Merino <alberto.rodriguez@altafonte.com>
2023-10-19 08:47:16 +0200

    fix query

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -439,7 +439,7 @@ class UserRepository extends ServiceEntityRepository implements UserLoaderInterf
                 ->andWhere('u.id NOT IN (SELECT IDENTITY(b.block_user) FROM App:BlockUser b WHERE b.from_user = :id)')
                 ->andWhere('u.id NOT IN (SELECT IDENTITY(bu.from_user) FROM App:BlockUser bu WHERE bu.block_user = :id)')
                 ->andWhere('u.id NOT IN (SELECT IDENTITY(h.hide_user) FROM App:HideUser h WHERE h.from_user = :id)')
-                ->andWhere("u.id IN (SELECT IDENTITY(t.user) FROM App:Tag t WHERE t.name LIKE :search OR t.slug = :search) OR u.name LIKE :search OR u.username LIKE :search")
+                ->andWhere("u.id IN (SELECT IDENTITY(t.user) FROM App:Tag t WHERE t.name LIKE :search OR t.slug LIKE :search) OR u.name LIKE :search OR u.username LIKE :search")
                 ->orderBy('distance', 'ASC')
                 ->addOrderBy('u.last_login', 'DESC')
                 ->setParameter('id', $user->getId())
